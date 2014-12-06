17be977ca1d1649ddd69d0d0194ca8af15cf87e6
Albert Rannetsperger <albert.rannetsperger@brightpearl.com>
2014-12-06 12:06:36 -0800

    More improvements to error output.

--- lib/convoy/trollop.rb
+++ lib/convoy/trollop.rb
@@ -408,14 +408,30 @@ module Trollop
 
                 case type
                     when :depends
-                        syms.each { |sym| raise CommandlineError, "--#{@specs[constraint_sym][:long]} requires --#{@specs[sym][:long]}" unless given_args.include? sym }
+                        # syms.each { |sym| raise CommandlineError, "--#{@specs[constraint_sym][:long]} requires --#{@specs[sym][:long]}" unless given_args.include? sym }
+                        syms.each { |sym|
+                            unless given_args.include? sym
+                                puts "\n  \x1B[48;5;196m ERROR \x1B[0m \xe2\x80\x94 --#{@specs[constraint_sym][:long]} requires --#{@specs[sym][:long]}\n\n"
+                                exit
+                            end
+                        }
                     when :conflicts
-                        syms.each { |sym| raise CommandlineError, "--#{@specs[constraint_sym][:long]} conflicts with --#{@specs[sym][:long]}" if given_args.include?(sym) && (sym != constraint_sym) }
+                        # syms.each { |sym| raise CommandlineError, "--#{@specs[constraint_sym][:long]} conflicts with --#{@specs[sym][:long]}" if given_args.include?(sym) && (sym != constraint_sym) }
+                        syms.each { |sym|
+                            if given_args.include?(sym) && (sym != constraint_sym)
+                                puts "\n  \x1B[48;5;196m ERROR \x1B[0m \xe2\x80\x94 --#{@specs[constraint_sym][:long]} conflicts with --#{@specs[sym][:long]}\n\n"
+                                exit
+                            end
+                        }
                 end
             end
 
             required.each do |sym, val|
-                raise CommandlineError, "option --#{@specs[sym][:long]} must be specified" unless given_args.include? sym
+                # raise CommandlineError, "option --#{@specs[sym][:long]} must be specified" unless given_args.include? sym
+                unless given_args.include? sym
+                    puts "\n  \x1B[48;5;196m ERROR \x1B[0m \xe2\x80\x94 option --#{@specs[sym][:long]} must be specified\n\n"
+                    exit
+                end
             end
 
             ## parse parameters
@@ -423,7 +439,10 @@ module Trollop
                 arg, params, negative_given = given_data.values_at :arg, :params, :negative_given
 
                 opts = @specs[sym]
-                raise CommandlineError, "option '#{arg}' needs a parameter" if params.empty? && opts[:type] != :flag
+
+                # raise CommandlineError, "option '#{arg}' needs a parameter" if params.empty? && opts[:type] != :flag
+                puts "\n  \x1B[48;5;196m ERROR \x1B[0m \xe2\x80\x94 Option needs a parameter: #{arg}\n\n" if params.empty? && opts[:type] != :flag
+                exit
 
                 vals["#{sym}_given".intern] = true # mark argument as specified on the commandline
 
